19dacf9793b533c5260564170db883e350d842ca
jisoo <doh_0112@naver.com>
2023-05-04 02:40:02 +0900

    요청 경로 상대경로로 수정

--- server/index.js
+++ server/index.js
@@ -19,29 +19,24 @@
 // });
 const jsonServer = require("json-server");
 const path = require("path");
-const express = require("express");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
-const middlewares = jsonServer.defaults();
-
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.use(express.static(path.join(__dirname, "..", "build")));
-
-app.use(middlewares);
-
-app.use(jsonServer.bodyParser);
-
-app.use("/api", router);
-
-// Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname, "../build/"),
 });
 
-server.use(app);
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+server.use("/api", router);
+
+server.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
 server.listen(port, () => {
   console.log("JSON Server is running");
