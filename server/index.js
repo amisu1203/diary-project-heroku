0d4650872c74423403793ea3841cb255ecb7c4fc
jisoo <doh_0112@naver.com>
2023-05-04 02:24:42 +0900

    라우팅 핸들링 코드 추가

--- server/index.js
+++ server/index.js
@@ -1,19 +1,48 @@
+// const jsonServer = require("json-server");
+// const path = require("path");
+
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
+// const middlewares = jsonServer.defaults({
+//   static: path.resolve(__dirname + "/../build/"),
+// });
+
+// const port = process.env.PORT || 3001;
+
+// server.use(middlewares);
+
+// server.use(jsonServer.bodyParser);
+
+// server.use(router);
+// server.listen(port, () => {
+//   console.log("JSON Server is running");
+// });
 const jsonServer = require("json-server");
 const path = require("path");
+const express = require("express");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
-const middlewares = jsonServer.defaults({
-  static: path.resolve(__dirname + "/../build/"),
-});
+const middlewares = jsonServer.defaults();
 
+const app = express();
 const port = process.env.PORT || 3001;
 
-server.use(middlewares);
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-server.use(jsonServer.bodyParser);
+app.use(middlewares);
 
-server.use(router);
+app.use(jsonServer.bodyParser);
+
+app.use("/api", router);
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+server.use(app);
+
 server.listen(port, () => {
   console.log("JSON Server is running");
 });
